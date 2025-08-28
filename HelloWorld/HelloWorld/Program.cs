f27fef01b45756d0c67262c61c1667ac01d440cc
JonCra <jonas.d.cram@gmail.com>
2025-08-28 11:48:08 -0600

    HelloWorld program

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,16 @@
+﻿//Jonas Cram
+//RCET 2265
+//Semester 4
+//Robotics and Comm. Sys. Engineering Technology
+//Repo URL: 
+namespace HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Console.Read();
+        }
+    }
+}
