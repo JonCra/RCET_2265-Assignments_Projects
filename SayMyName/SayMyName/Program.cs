dcfa9214096188e7a3f2258b0007ad8b80afb34a
JonCra <jonas.d.cram@gmail.com>
2025-09-02 12:45:25 -0600

    Added assignment header (name, program, semester, etc.)

---
+++ SayMyName/SayMyName/Program.cs
@@ -0,0 +1,16 @@
+﻿//Jonas Cram
+//RCET 2265
+//Fall 2025
+//Robotics and Comm. Sys. Engineering Tech.
+//
+
+namespace SayMyName
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
