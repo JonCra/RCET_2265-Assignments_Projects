54139a2472f83c93c7ac4d5c73b3f98635fa9c21
JonCra <jonas.d.cram@gmail.com>
2025-09-09 08:03:32 -0600

    added if and else if conditionals for control

--- SayMyName/SayMyName/Program.cs
+++ SayMyName/SayMyName/Program.cs
@@ -2,7 +2,7 @@
 //RCET 2265
 //Fall 2025
 //Robotics and Comm. Sys. Engineering Tech.
-//
+//https://github.com/JonCra/SayMyName.git
 
 namespace SayMyName
 {
@@ -10,7 +10,20 @@ namespace SayMyName
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //Asks user for their name
+            Console.WriteLine("What is your name?");
+
+            //Reads user's name
+            Console.WriteLine(${ Console.WriteLine()} "Your name is {Console.ReadLine()}.");
+
+            //Adds visual gap
+            Console.WriteLine();
+
+            //Tells user how to exit program
+            Console.WriteLine("Press 'Enter' to quit");
+
+            //Used as a "pause" before exiting program
+            Console.ReadLine();
         }
     }
 }
---
+++ SimpleCalculator_v2/SimpleCalculator_v2/Program.cs
@@ -0,0 +1,85 @@
+﻿//Jonas Cram
+//RCET 2265
+//Fall 2025
+//Robotics and Comm. Sys. Engineering Tech.
+//TODO: add GitHub URL
+
+using System.Data;
+
+namespace SimpleCalculator_v2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Text Data
+            nint firstNumber;
+            nint secondNumber;
+            nint result_1;
+            nint result_2;
+            nint operationSelect;
+
+
+            Console.WriteLine("Enter a number.");
+            firstNumber = Console.ReadLine();
+            //Display first entered value
+            Console.WriteLine($"Number 1: {firstNumber}");
+
+
+            //Add visual break
+            Console.WriteLine();
+
+
+            Console.WriteLine("Enter another number.");
+            secondNumber = Console.ReadLine();
+            //Display second entered value
+            Console.WriteLine($"Number 2: {secondNumber}");
+
+
+
+
+            //Add visual break
+            Console.WriteLine();
+
+            //Ask user for operation type
+            Console.WriteLine("Select Operation:");
+            Console.WriteLine($"1. Product ({firstNumber} * {secondNumber})");
+            Console.WriteLine($"2. Sum ({firstNumber} + {secondNumber})");
+
+            //TODO: Error is a conversion type... It's formatted as a string, but needs to be a numerical "base". Fix it.
+            result_1 = firstNumber * secondNumber;
+            Console.WriteLine($"{firstNumber} * {secondNumber} = {result_1}");
+
+            result_2 = firstNumber + secondNumber;
+            Console.WriteLine($"{firstNumber} + {secondNumber} = {result_2}");
+
+
+            operationSelect = Console.ReadLine();
+
+            if (operationSelect == 1)
+            {
+                Console.WriteLine(result_1);
+            }
+
+            else if (operationSelect == 2)
+            {
+                Console.WriteLine(result_2);
+            }
+
+            else (operationSelect < 1 || 2 <)
+            {
+                Console.WriteLine("Error. Please enter a valid number.");
+            }
+
+
+            //Add visual break
+            Console.WriteLine();
+
+            //Add delay
+            Console.WriteLine("Press 'Enter' to exit");
+            Console.ReadLine();
+
+        }
+    }
+}
