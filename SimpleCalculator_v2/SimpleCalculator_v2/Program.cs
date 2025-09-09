a567c1faf2af28618d285978456c59a97ac967be
JonCra <jonas.d.cram@gmail.com>
2025-09-09 14:17:55 -0600

    Fixed potential issues

--- SimpleCalculator_v2/SimpleCalculator_v2/Program.cs
+++ SimpleCalculator_v2/SimpleCalculator_v2/Program.cs
@@ -18,68 +18,68 @@ namespace SimpleCalculator_v2
             nint secondNumber;
             nint result_1;
             nint result_2;
-            nint operationSelect;
+            string operationSelect;
+            string userInput;
 
-
-            Console.WriteLine("Enter a number.");
-            firstNumber = Console.ReadLine();
-            //Display first entered value
-            Console.WriteLine($"Number 1: {firstNumber}");
-
-
-            //Add visual break
-            Console.WriteLine();
-
-
-            Console.WriteLine("Enter another number.");
-            secondNumber = Console.ReadLine();
-            //Display second entered value
-            Console.WriteLine($"Number 2: {secondNumber}");
-
-
-
-
-            //Add visual break
-            Console.WriteLine();
-
-            //Ask user for operation type
-            Console.WriteLine("Select Operation:");
-            Console.WriteLine($"1. Product ({firstNumber} * {secondNumber})");
-            Console.WriteLine($"2. Sum ({firstNumber} + {secondNumber})");
-
-            //TODO: Error is a conversion type... It's formatted as a string, but needs to be a numerical "base". Fix it.
-            result_1 = firstNumber * secondNumber;
-            Console.WriteLine($"{firstNumber} * {secondNumber} = {result_1}");
-
-            result_2 = firstNumber + secondNumber;
-            Console.WriteLine($"{firstNumber} + {secondNumber} = {result_2}");
-
-
-            operationSelect = Console.ReadLine();
-
-            if (operationSelect == 1)
+            do
             {
-                Console.WriteLine(result_1);
-            }
-
-            else if (operationSelect == 2)
-            {
-                Console.WriteLine(result_2);
-            }
-
-            else (operationSelect < 1 || 2 <)
-            {
-                Console.WriteLine("Error. Please enter a valid number.");
-            }
+                Console.WriteLine("Enter a number.");
+                firstNumber = Console.ReadLine();
+                //Display first entered value
+                Console.WriteLine($"Number 1: {firstNumber}");
 
 
-            //Add visual break
-            Console.WriteLine();
+                //Add visual break
+                Console.WriteLine();
 
-            //Add delay
-            Console.WriteLine("Press 'Enter' to exit");
-            Console.ReadLine();
 
+                Console.WriteLine("Enter another number.");
+                secondNumber = Console.ReadLine();
+                //Display second entered value
+                Console.WriteLine($"Number 2: {secondNumber}");
+
+
+                //Add visual break
+                Console.WriteLine();
+
+
+                //Ask user for operation type
+                Console.WriteLine("Select Operation:");
+                Console.WriteLine($"1. Product ({firstNumber} * {secondNumber})");
+                Console.WriteLine($"2. Sum ({firstNumber} + {secondNumber})");
+
+                //TODO: Error is a conversion type... It's formatted as a string, but needs to be a numerical "base". Fix it.
+                result_1 = firstNumber * secondNumber;
+                Console.WriteLine($"{firstNumber} * {secondNumber} = {result_1}");
+
+                result_2 = firstNumber + secondNumber;
+                Console.WriteLine($"{firstNumber} + {secondNumber} = {result_2}");
+
+
+                operationSelect = Console.ReadLine();
+
+                if (operationSelect == "1")
+                {
+                    Console.WriteLine($"{firstNumber} * {secondNumber} = {result_1}");
+                }
+
+                else if (operationSelect == "2")
+                {
+                    Console.WriteLine($"{firstNumber} + {secondNumber} = {result_2}");
+                }
+
+                else
+                {
+                    Console.WriteLine("Error. Please enter a valid number.");
+                }
+
+
+                //Add visual break
+                Console.WriteLine();
+
+                Console.WriteLine($"Press 'q' to exit, or any other key to run again");
+                                
+            } while (userInput != "q");
         }
     }
 }
