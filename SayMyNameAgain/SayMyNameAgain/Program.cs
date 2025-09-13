3dac23c0fc1d9ff44fb55a1747d494748b592727
JonCra <jonas.d.cram@gmail.com>
2025-09-13 14:16:24 -0600

    working on minor bug fixes

--- SayMyNameAgain/SayMyNameAgain/Program.cs
+++ SayMyNameAgain/SayMyNameAgain/Program.cs
@@ -10,37 +10,62 @@ namespace SayMyNameAgain
 
             string userName = "";
             string userInput = "";
+            string timInput = "";
 
             Console.WriteLine("What is your name?");
             userName = Console.ReadLine();
 
             do
             {
-
-
-                if (userName == "Joe")
+                Console.WriteLine("Press 'q' to quit at any time\n");
+                if (userName == "Joe" || userName == "joe")
                 {
                     Console.WriteLine("Howdy, Joe! How's Susie?");
+                    Console.ReadLine();
                 }
-                else if (userName == "Emily")
+                else if (userName == "Emily" || userName == "emily")
                 {
                     Console.WriteLine("Hi, Emily! How's the dogs?");
+                    Console.ReadLine();
                 }
-                else if (userName == "Jonas")
+                else if (userName == "Jonas" || userName == "jonas")
                 {
                     Console.WriteLine("Hey, Jonas! C# is fun, isn't it?");
+                    Console.ReadLine();
                 }
-                else if (userName == "Tim" || userName == "Timothy")
+                else if (userName == "Tim" || userName == "tim")
                 {
-                    Console.WriteLine("Hello, Tim! Thinking about giving me an A for this assignment? :D");
-                }
-                else
-                {
-                    Console.WriteLine($"Hello, {userName}");
-                }
-            } while (userInput != "q");
-            
+                    Console.Write("Heyyyy, Tim! Wanna give me an A for this assignment?\n" +
+                        "y/n\n");
 
+                    timInput = Console.ReadLine();
+                    if (timInput == "y")
+                    {
+                        Console.WriteLine("YEEEESSSSSS :D");
+                        userInput = Console.ReadLine();
+                    }
+                    else if (timInput == "n")
+                    {
+                        Console.WriteLine("Ehhh... worth a shot ;-;");
+                        userInput = Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine($"You chose poorly...\n\n" +
+                            "Deleting files\n" +
+                            "C:/Windows/system32...");
+                        userInput = Console.ReadLine();
+                    }
+
+                else
+                    {
+                        Console.WriteLine($"Hello, {userName}.");
+                        userInput = Console.ReadLine();
+                    }
+                } while (userInput != "q") ;
+
+
+            } while (userInput != "q");
         }
-    }
+    } 
 }
