52f42fd06286745797aa8dce9d9eb4032e170972
JonCra <jonas.d.cram@gmail.com>
2025-09-14 15:01:07 -0600

    added GitHub URL

--- SayMyNameAgain/SayMyNameAgain/Program.cs
+++ SayMyNameAgain/SayMyNameAgain/Program.cs
@@ -6,7 +6,12 @@ namespace SayMyNameAgain
     {
         static void Main(string[] args)
         {
-            //TODO Enter repo URL here
+            //
+            //Jonas Cram
+            //RCET 2265
+            //Fall 2025
+            //Robotics and Comm. Sys. Engineering Tech.
+            //TODO: https://github.com/JonCra/RCET_2265-Assignments_Projects/tree/fa0ea4834dc3d16e1c302b91000ba961daeb470c/SayMyNameAgain
 
             string userName = "";
             string timInput = "";
