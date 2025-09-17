09ff9690d66ab6e8fc575a2f3bf3b8659073ec0e
JonCra <jonas.d.cram@gmail.com>
2025-09-17 14:50:53 -0600

    added fortunes and TODO for random number gen

--- FortuneCookie/FortuneCookie/Program.cs
+++ FortuneCookie/FortuneCookie/Program.cs
@@ -3,7 +3,7 @@
 //RCET 2265
 //Fall 2025
 //Robotics and Comm. Sys. Engineering Tech.
-//TODO: Add GitHub URL
+//https://github.com/JonCra/RCET_2265-Assignments_Projects/tree/3c11c303950eded48c6825826adc3fac8d94586f/FortuneCookie
 
 namespace FortuneCookie
 {
@@ -11,8 +11,14 @@ namespace FortuneCookie
     {
         static void Main(string[] args)
         {
-            
+            string fortZero = "Yesterday is history, tomorrow is a mystery, but today is a gift..."
+                + "\nThat is why it is called the Present.";
+            string fortOne = "If a situation is out of your hands, why is it in your head?";
+            string fortTwo = "A candle loses nothing by lighting another candle.";
+            string fortThree = "You can't control the wind, but you can adjust your sails.";
+            string fortFive = "The best revenge is to not be like your enemy.";
 
+            //TODO: insert/finalize random roll checker (0-5 or 1-6)
 
 
         }
