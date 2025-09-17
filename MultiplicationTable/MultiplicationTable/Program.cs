c74e1372ecbc20295daf23e152e5eb01837331a1
JonCra <jonas.d.cram@gmail.com>
2025-09-17 07:51:08 -0600

    Finalized and added documentation

--- MultiplicationTable/MultiplicationTable/Program.cs
+++ MultiplicationTable/MultiplicationTable/Program.cs
@@ -3,7 +3,7 @@
 //RCET 2265
 //Fall 2025
 //Robotics and Comm. Sys. Engineering Tech.
-//TODO: Add GitHub URL
+//https://github.com/JonCra/RCET_2265-Assignments_Projects/tree/66e17e39bb5b147fb563cd94eab03d19c7985f85/MultiplicationTable
 
 namespace MultiplicationTable
 {
@@ -11,19 +11,23 @@ namespace MultiplicationTable
     {
         static void Main(string[] args)
         {
+            //Acts like an x/y graph, 12 values left/down
             int x = 12;
             int y = 12;
 
-
+            //Each value starts at 1 and will stop when the iteration is NOT less than 13... i.e: 12
             for (x = 1; x < 13; x++)
             {
                 for (y = 1; y < 13; y++)
                 {
+                    //Multiplies each value together, and "pads" each sequence with 3 spaces and a divider
                     Console.Write((x * y).ToString().PadLeft(3) + " | ");
-
                 }
+                //Breaks after each sequence
                 Console.WriteLine();
             }
+                //Pause
+                Console.ReadLine();            
         }
     }
 }
