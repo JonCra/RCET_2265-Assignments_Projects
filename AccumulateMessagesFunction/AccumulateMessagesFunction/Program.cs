0527813b626daa174265318108ba8c51c5e10b1c
JonCra <jonas.d.cram@gmail.com>
2025-09-23 08:01:35 -0600

    initial push

---
+++ AccumulateMessagesFunction/AccumulateMessagesFunction/Program.cs
@@ -0,0 +1,18 @@
+﻿/*
+    Jonas Cram
+    RCET 2265
+    Fall 2025
+    Robotics and Comm. Sys. Engineering Tech.
+    https://github.com/JonCra/RCET_2265-Assignments_Projects.git
+ */
+
+namespace AccumulateMessagesFunction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
