60fdc45b1eb59e048a23a118a46792405973188e
JonCra <jonas.d.cram@gmail.com>
2025-09-27 10:13:20 -0600

    Transfered code from lecture

--- AccumulateMessagesFunction/AccumulateMessagesFunction/Program.cs
+++ AccumulateMessagesFunction/AccumulateMessagesFunction/Program.cs
@@ -8,11 +8,89 @@
 
 namespace AccumulateMessagesFunction
 {
+    /*
+    TODO:
+        Define a function that takes a string message and appends it to an accumulating result (e.g., a log or transcript).
+        Allow the user to enter multiple messages, accumulating them until the user chooses to stop.
+        Display the accumulated messages at the end.
+        Wait for the user to press Enter before closing the console window.
+    */
     internal class Program
     {
-        static void Main(string[] args)
+        private string _messages = "";
+        static void TestMessagesManual()
         {
-            
+            string userMessage = "this is important";
+            bool userHasQuit = false;
+            do
+            {
+                Console.WriteLine("Press 'q' or 'Q' any time to quit.\n" +
+                    "Enter a message:\n");
+                userMessage = Console.ReadLine();
+                if (userMessage == "q" || userMessage == "Q")
+                {
+                    userHasQuit = true;
+                }
+                else if (userMessage == "m" || userMessage == "M")
+                {
+                    Console.Clear();
+                    Console.WriteLine("The Messages are:\n\n" + Messages() + "\n");
+                }
+                else if (userMessage == "c" || userMessage == "C")
+                {
+                    //clear all messages
+                    Messages("", true);
+                    //TODO add clear flag                    
+                }
+                else
+                {
+                    Messages(userMessage);
+                    Console.Clear();
+                }
+            } while (userHasQuit == false);
+
+            Console.Clear();
+            Console.WriteLine("The messages are:\n\n" + Messages());
+            Console.WriteLine("\nPress 'enter' to quit.");
+        }
+        static string Messages(string currentMessage = "", bool clear = false)
+        {
+            //removes leading/lagging whitespace
+            currentMessage = currentMessage.Trim();
+
+            //Grab the contents of the class level static variable
+            string messages = _messages;
+            if (clear == true)
+            {
+                messages = "";
+            }
+            //test for "", skip if "" - skips blank lines
+            else if (messages.Length == 0 && currentMessage != "")//avoid initial blank line
+            {
+                //append currrent message
+                messages += currentMessage;
+            }
+            else if (currentMessage != "")//skip empty messages
+            {
+                //Append new line and the current message
+                messages += "\n" + currentMessage;
+            }
+
+            //Store the updated messages to global
+            _messages = messages;
+
+            //Return the messages
+            return messages;
+        }
+        static void TestMessages()
+        {
+            Messages("Hello!");
+            Messages("Bye!");
+            Messages("");
+            Messages("Punch!");
+            Messages("Pie!");
+            Messages("Dog!");
+            Console.WriteLine(Messages("Pony"));
         }
     }
 }
