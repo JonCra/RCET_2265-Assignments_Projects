986ab902812810de6c7858e4b93b20ba78c04947
JonCra <jonas.d.cram@gmail.com>
2025-10-01 08:01:33 -0600

    add TODO list

---
+++ RollOfTheDice/RollOfTheDice/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace RollOfTheDice
+{
+    /*
+     TODO:
+        [ ] Display the results in the console in formatted columns with headers.
+        [ ] The header row should show the roll result numbers 2 to 12.
+        [ ] The data row should show the total count of how many times each number came up.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Roll of the Dice");
+        }
+    }
+}
