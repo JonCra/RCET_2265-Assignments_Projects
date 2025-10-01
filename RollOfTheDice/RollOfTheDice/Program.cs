4dc69349c61878fa0b9a1882f91a970b049a49ce
JonCra <jonas.d.cram@gmail.com>
2025-10-01 08:04:35 -0600

    added different methods with respective TODO lists

--- RollOfTheDice/RollOfTheDice/Program.cs
+++ RollOfTheDice/RollOfTheDice/Program.cs
@@ -3,7 +3,7 @@
     /*
      TODO:
         [ ] Display the results in the console in formatted columns with headers.
-        [ ] The header row should show the roll result numbers 2 to 12.
+        [ ] The header row should show the roll result numbers 2 to 12 (2d6).
         [ ] The data row should show the total count of how many times each number came up.
      */
     internal class Program
@@ -11,6 +11,19 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Roll of the Dice");
+        }
+        static void Display()
+        {
+            //TODO
+            //Format results in a "table" - Make it look good
+            //Display results
+                //Display rolls 2-12
+                //Display roll results
+        }
+        static void DiceRoller()
+        {
+            //Roll two 1d6 Die
+            //tally the total sum of each roll
         }
     }
 }
