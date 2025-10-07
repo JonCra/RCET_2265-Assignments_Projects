c3950ee2a940e129fad7dce7594b0e1bf3573108
JonCra <jonas.d.cram@gmail.com>
2025-10-07 07:54:23 -0600

    initial push

---
+++ ShuffleTheDeck/ShuffleTheDeck/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+   Jonas Cram
+   RCET 2265
+   Fall 2025
+   Robotics and Comm. Sys. Engineering Tech.
+   https://github.com/JonCra/RCET_2265-Assignments_Projects.git
+*/
+namespace ShuffleTheDeck
+{
+    /*
+     TODO:
+        [ ] Create a 52 card deck to draw from
+        [ ] The card should be a random suit (spades, clubs, hearts, diamonds) and value (2-10, J, Q, K, A).
+        [ ] Use a multidimensional array to track if the card has already been dealt. If so, draw another random card.
+        [ ] Shuffle the deck when there are no more cards left or anytime the user chooses.
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
