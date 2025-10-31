2a11e77ad72ada8d9a5b8d803f01e6b22a8ad312
JonCra <jonas.d.cram@gmail.com>
2025-10-31 14:06:24 -0600

    added structures for program function

--- DinerMenu_Fixed/DinerMenu_Fixed/Diner Menu.Designer.cs
+++ DinerMenu_Fixed/DinerMenu_Fixed/Diner Menu.Designer.cs
@@ -28,18 +28,74 @@
         /// </summary>
         private void InitializeComponent()
         {
+            components = new System.ComponentModel.Container();
+            toolTip1 = new ToolTip(components);
+            ExitButton = new Button();
+            SoupButton = new Button();
+            SaladButton = new Button();
+            FishButton = new Button();
             SuspendLayout();
+            // 
+            // ExitButton
+            // 
+            ExitButton.Location = new Point(676, 387);
+            ExitButton.Name = "ExitButton";
+            ExitButton.Size = new Size(112, 34);
+            ExitButton.TabIndex = 0;
+            ExitButton.Text = "E&xit";
+            ExitButton.UseVisualStyleBackColor = true;
+            ExitButton.Click += ExitButton_Click;
+            // 
+            // SoupButton
+            // 
+            SoupButton.Location = new Point(12, 387);
+            SoupButton.Name = "SoupButton";
+            SoupButton.Size = new Size(112, 34);
+            SoupButton.TabIndex = 1;
+            SoupButton.Text = "Soup";
+            SoupButton.UseVisualStyleBackColor = true;
+            SoupButton.Click += SoupButton_Click;
+            // 
+            // SaladButton
+            // 
+            SaladButton.Location = new Point(177, 387);
+            SaladButton.Name = "SaladButton";
+            SaladButton.Size = new Size(112, 34);
+            SaladButton.TabIndex = 2;
+            SaladButton.Text = "Salad";
+            SaladButton.UseVisualStyleBackColor = true;
+            SaladButton.Click += SaladButton_Click;
+            // 
+            // FishButton
+            // 
+            FishButton.Location = new Point(340, 387);
+            FishButton.Name = "FishButton";
+            FishButton.Size = new Size(112, 34);
+            FishButton.TabIndex = 3;
+            FishButton.Text = "Fish";
+            FishButton.UseVisualStyleBackColor = true;
+            FishButton.Click += FishButton_Click;
             // 
             // Form1
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(FishButton);
+            Controls.Add(SaladButton);
+            Controls.Add(SoupButton);
+            Controls.Add(ExitButton);
             Name = "Form1";
             Text = "Ma & Pa's Corner Diner";
             ResumeLayout(false);
         }
 
         #endregion
+
+        private ToolTip toolTip1;
+        private Button ExitButton;
+        private Button SoupButton;
+        private Button SaladButton;
+        private Button FishButton;
     }
 }
--- DinerMenu_Fixed/DinerMenu_Fixed/Diner Menu.cs
+++ DinerMenu_Fixed/DinerMenu_Fixed/Diner Menu.cs
@@ -5,6 +5,47 @@ namespace DinerMenu_Fixed
         public Form1()
         {
             InitializeComponent();
+            
+            // Ensures "menu" is empty on startup
+        }
+
+        // Program Logic -------------------------------------------
+        
+        void SoupDisplay()
+        {
+            // Display Soup menu
+
+        }
+        void SaladDisplay()
+        {
+            // Display Salad menu
+
+        }
+        void FishDisplay()
+        {
+            // Display Fish menu
+
+        }
+
+        // Event Handlers ------------------------------------------
+        private void FishButton_Click(object sender, EventArgs e)
+        {
+            // Display Fish menu
+            FishDisplay();
+        }
+        private void ExitButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void SaladButton_Click(object sender, EventArgs e)
+        {
+            // Display Salad menu
+            SaladDisplay();
+        }
+        private void SoupButton_Click(object sender, EventArgs e)
+        {
+            // Display Soup menu
+            SoupDisplay();
         }
     }
 }
