b131ec529cd6de792c7b0caf6b1776c9e3382908
JonCra <jonas.d.cram@gmail.com>
2025-11-09 15:40:15 -0700

    Added name/header format to program/.cs file

--- RollOfTheDice_ListBox/RollOfTheDice_ListBox/RollOfTheDiceForm.cs
+++ RollOfTheDice_ListBox/RollOfTheDice_ListBox/RollOfTheDiceForm.cs
@@ -1,3 +1,11 @@
+/*
+Jonas Cram
+RCET 2265
+Fall 2025
+Robotics and Comm. Systems Engineering Technology
+https://github.com/JonCra/RCET_2265-Assignments_Projects
+ */
+
 namespace RollOfTheDice_ListBox
 {
     public partial class RollOfTheDiceForm : Form
