99a9af562b0a19746003f89a5ec67a9fd99f64dc
JonCra <jonas.d.cram@gmail.com>
2025-11-12 13:24:26 -0700

    adding things to GUI

--- CarRental/CarRental/CarRentalForm.Designer.cs
+++ CarRental/CarRental/CarRentalForm.Designer.cs
@@ -30,70 +30,126 @@
         {
             components = new System.ComponentModel.Container();
             toolTip1 = new ToolTip(components);
-            CalculateButton = new Button();
             ClearButton = new Button();
             SummaryButton = new Button();
-            ExitButton = new Button();
+            CalculateButton = new Button();
+            button2 = new Button();
+            DiscountGroupBox = new GroupBox();
+            AAA_DiscountCheckBox = new CheckBox();
+            SeniorDiscountCheckBox = new CheckBox();
+            OdometerUnitGroupBox = new GroupBox();
+            DiscountGroupBox.SuspendLayout();
             SuspendLayout();
-            // 
-            // CalculateButton
-            // 
-            CalculateButton.Location = new Point(228, 377);
-            CalculateButton.Name = "CalculateButton";
-            CalculateButton.Size = new Size(112, 34);
-            CalculateButton.TabIndex = 0;
-            CalculateButton.Text = "Calulate";
-            CalculateButton.UseVisualStyleBackColor = true;
             // 
             // ClearButton
             // 
-            ClearButton.Location = new Point(416, 381);
+            ClearButton.Location = new Point(380, 459);
             ClearButton.Name = "ClearButton";
             ClearButton.Size = new Size(112, 34);
             ClearButton.TabIndex = 1;
             ClearButton.Text = "Clear";
             ClearButton.UseVisualStyleBackColor = true;
+            ClearButton.Click += ClearButton_Click;
             // 
             // SummaryButton
             // 
-            SummaryButton.Location = new Point(566, 343);
+            SummaryButton.Location = new Point(262, 459);
             SummaryButton.Name = "SummaryButton";
             SummaryButton.Size = new Size(112, 34);
             SummaryButton.TabIndex = 2;
             SummaryButton.Text = "Summary";
             SummaryButton.UseVisualStyleBackColor = true;
+            SummaryButton.Click += SummaryButton_Click;
             // 
-            // ExitButton
+            // CalculateButton
             // 
-            ExitButton.Location = new Point(596, 268);
-            ExitButton.Name = "ExitButton";
-            ExitButton.Size = new Size(112, 34);
-            ExitButton.TabIndex = 3;
-            ExitButton.Text = "Exit";
-            ExitButton.UseVisualStyleBackColor = true;
-            ExitButton.Click += ExitButton_Click;
+            CalculateButton.Location = new Point(144, 459);
+            CalculateButton.Name = "CalculateButton";
+            CalculateButton.Size = new Size(112, 34);
+            CalculateButton.TabIndex = 0;
+            CalculateButton.Text = "Calculate";
+            CalculateButton.UseVisualStyleBackColor = true;
+            CalculateButton.Click += CalculateButton_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(498, 459);
+            button2.Name = "button2";
+            button2.Size = new Size(112, 34);
+            button2.TabIndex = 3;
+            button2.Text = "Exit";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += ExitButton_Click;
+            // 
+            // DiscountGroupBox
+            // 
+            DiscountGroupBox.Controls.Add(SeniorDiscountCheckBox);
+            DiscountGroupBox.Controls.Add(AAA_DiscountCheckBox);
+            DiscountGroupBox.Location = new Point(488, 158);
+            DiscountGroupBox.Name = "DiscountGroupBox";
+            DiscountGroupBox.Size = new Size(300, 150);
+            DiscountGroupBox.TabIndex = 4;
+            DiscountGroupBox.TabStop = false;
+            DiscountGroupBox.Text = "Discounts";
+            // 
+            // AAA_DiscountCheckBox
+            // 
+            AAA_DiscountCheckBox.AutoSize = true;
+            AAA_DiscountCheckBox.Location = new Point(35, 38);
+            AAA_DiscountCheckBox.Name = "AAA_DiscountCheckBox";
+            AAA_DiscountCheckBox.Size = new Size(149, 29);
+            AAA_DiscountCheckBox.TabIndex = 0;
+            AAA_DiscountCheckBox.Text = "AAA Discount";
+            AAA_DiscountCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // SeniorDiscountCheckBox
+            // 
+            SeniorDiscountCheckBox.AutoSize = true;
+            SeniorDiscountCheckBox.Location = new Point(34, 87);
+            SeniorDiscountCheckBox.Name = "SeniorDiscountCheckBox";
+            SeniorDiscountCheckBox.Size = new Size(227, 29);
+            SeniorDiscountCheckBox.TabIndex = 1;
+            SeniorDiscountCheckBox.Text = "Senior Citizen Checkbox";
+            SeniorDiscountCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // OdometerUnitGroupBox
+            // 
+            OdometerUnitGroupBox.Location = new Point(34, 218);
+            OdometerUnitGroupBox.Name = "OdometerUnitGroupBox";
+            OdometerUnitGroupBox.Size = new Size(339, 150);
+            OdometerUnitGroupBox.TabIndex = 5;
+            OdometerUnitGroupBox.TabStop = false;
+            OdometerUnitGroupBox.Text = "Is Odometer in Miles or Kilometers?";
             // 
             // CarRentalForm
             // 
             AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(ExitButton);
+            ClientSize = new Size(800, 519);
+            Controls.Add(OdometerUnitGroupBox);
+            Controls.Add(DiscountGroupBox);
+            Controls.Add(button2);
             Controls.Add(SummaryButton);
-            Controls.Add(ClearButton);
             Controls.Add(CalculateButton);
+            Controls.Add(ClearButton);
             Name = "CarRentalForm";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "Form1";
+            DiscountGroupBox.ResumeLayout(false);
+            DiscountGroupBox.PerformLayout();
             ResumeLayout(false);
         }
 
         #endregion
 
         private ToolTip toolTip1;
-        private Button CalculateButton;
         private Button ClearButton;
         private Button SummaryButton;
-        private Button ExitButton;
+        private Button CalculateButton;
+        private Button button2;
+        private GroupBox DiscountGroupBox;
+        private CheckBox SeniorDiscountCheckBox;
+        private CheckBox AAA_DiscountCheckBox;
+        private GroupBox OdometerUnitGroupBox;
     }
 }
--- CarRental/CarRental/CarRentalForm.cs
+++ CarRental/CarRental/CarRentalForm.cs
@@ -5,15 +5,35 @@ namespace CarRental
         public CarRentalForm()
         {
             InitializeComponent();
+            SetDefaults();
         }
 
         // Program Logic -----------------------------------------
+        private void SetDefaults()
+        {
+
+        }
 
 
         // Event Handlers ----------------------------------------
+        private void CalculateButton_Click(object sender, EventArgs e)
+        {
+            // Calculate based on user input
+        }
+
         private void ExitButton_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void ClearButton_Click(object sender, EventArgs e)
+        {
+            SetDefaults();
+        }
+
+        private void SummaryButton_Click(object sender, EventArgs e)
+        {
+            // Display summary in pop up
         }
     }
 }
