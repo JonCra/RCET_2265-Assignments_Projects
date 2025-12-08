604fcd87c99ea0123241737fcff8b442e96dad3d
JonCra <jonas.d.cram@gmail.com>
2025-12-08 08:26:08 -0700

    init commit; Added assignment header, changed form title, outlined program structure

---
+++ Stan'sGrocery/Stan'sGrocery/Form1.cs
@@ -0,0 +1,32 @@
+/*
+ * Jonas Cram
+ * RCET 2265
+ * Fall 2025
+ * Robotics and Comm. Systems Engineering Technology
+ * https://github.com/JonCra/RCET_2265-Assignments_Projects.git
+ */
+namespace Stan_sGrocery
+{
+    public partial class GrocerySearchForm : Form
+    {
+        public GrocerySearchForm()
+        {
+            InitializeComponent();
+        }
+
+        // "Global" Variables ---------------
+
+
+        // Initializers ---------------------
+        private void SetDefaults()
+        {
+
+        }
+
+        // Program Logic --------------------
+
+
+        // Event Handlers -------------------
+
+    }
+}
