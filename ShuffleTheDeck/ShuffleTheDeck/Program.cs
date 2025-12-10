266957f05516afc047dd90d9c6ac9166999f2f6e
JonCra <jonas.d.cram@gmail.com>
2025-12-09 21:29:26 -0700

    Fixed error where cards would not display correct value; need to correct the padding of the suit header

--- ShuffleTheDeck/ShuffleTheDeck/Program.cs
+++ ShuffleTheDeck/ShuffleTheDeck/Program.cs
@@ -12,7 +12,7 @@ namespace ShuffleTheDeck
         [X] Create a 52 card deck to draw from
         [ ] The card should be a random suit (spades, clubs, hearts, diamonds) and value (2-10, J, Q, K, A).
         [X] Use a multidimensional array to track if the card has already been dealt. If so, draw another random card.
-        [ ] Shuffle the deck when there are no more cards left or anytime the user chooses.
+        [X] Shuffle the deck when there are no more cards left or anytime the user chooses.
      */
     internal class Program
     {
@@ -36,17 +36,19 @@ namespace ShuffleTheDeck
                 {
                     Console.WriteLine($"You drew {NameOfCard}");
                 }
+
                 userInput = Console.ReadLine();
 
                 if (userInput == "q" || userInput == "Q")
                 {
+                    // Quit function
                     Console.WriteLine("Have a nice day!");
                     Console.ReadLine();
                     userQuit = true;
                 }
                 else if (userInput == "c" || userInput == "C")
                 {
-                    //Restart
+                    // Restart/clear function
                     NewDeck();
                     count = 0;
                     userQuit = false;
@@ -77,6 +79,7 @@ namespace ShuffleTheDeck
                         userQuit = false;
                     }
                 }
+                // Clears and updates
                 Console.Clear();
             } while (!userQuit);
         }
@@ -98,18 +101,18 @@ namespace ShuffleTheDeck
 
         static void DrawCard()
         {
-            int column = 0;
-            int row = 0;
+            int column;
+            int row;
             do
             {
-                //Get random column
+                // Get random column
                 column = RandomSuitBetween(0, 3);
 
-                //Get random row
+                // Get random row
                 row = RandomSuitBetween(0, 12);
             } while (deck[row, column] == true);
 
-            //Mark drawn card as true
+            // Mark drawn card as true
             deck[row, column] = true;
 
             string cardName = $"{cardValue[row]} of {cardValue[column]}";
@@ -118,9 +121,10 @@ namespace ShuffleTheDeck
         
         static void NewDeck()
         {
-            //Create a local array that has the same type, dimensions, size
+            // Create a local array that has the same type, dimensions, size
             bool[,] empty = new bool[13, 4];
-            //Overwrite the tracker array with the default contents of the temp array
+
+            // Overwrite the tracker array with the default contents of the temp array
             deck = empty;
         }
         
@@ -129,6 +133,12 @@ namespace ShuffleTheDeck
             Random RandomNumber = new Random();
             return RandomNumber.Next(min, max + 1);
         }
+
+        //static string RandomCardBetween(string A, string K)
+        //{
+        //    Random RandomCard = new Random();
+        //    return RandomCard.Next(A, K);
+        //}
         
         static void Display()
         {
@@ -137,16 +147,27 @@ namespace ShuffleTheDeck
             string vDivider = " |"; //Column seperator
             int totalWidth = 5 * (padding + vDivider.Length);   //Calculate total width
             string hDivider = new string('-', totalWidth);  //Dynamic length horizontal divider
-                        
-            //Display UI
+
+            for (int i = 0; i < cardSuit.Length; i++)
+            {
+                Console.Write(cardSuit[i]);
+                
+                if (i < 3)
+                {
+                    Console.Write(" | ");
+                }
+            }
+            Console.WriteLine();
+
+            // Display card matrix
             for (int row = 0; row < 13; row++)
             {
                 for (int column = 0; column < 4; column++)
                 {
                     if (CardIsDrawn(row, column))
                     {
-                        //currentLocation = "00"; //TODO resolve actual number
-                        currentLocation = ((column * 13) + row + 1).ToString();
+                        // Show actual card value and suit
+                        currentLocation = $"{cardValue[row]}";
                     }
                     else
                     {
